19380cfe6c0a656c64ce47013d61317529b48a1d
sardem20 <mateusdev2@gmail.com>
2026-08-27 19:40:29 -0300

    Delete IntelligeCar directory

--- IntelligeCar/backend/services/veiculos/buscar_veiculo_service.py
+++
@@ -1,6 +0,0 @@
-from models.veiculo import Veiculo
-
-class BuscarVeiculoService:
-    def execute(self, veiculo_id):
-        veiculo = Veiculo.buscar_por_id(veiculo_id)
-        return veiculo.to_dict() if veiculo else None
